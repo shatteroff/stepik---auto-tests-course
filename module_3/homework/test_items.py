12dc2b8061007747027c5dfd832dbce56060977b
andrei.ursaki <shatter007@mail.ru>
2019-11-25 16:52:11 +0300

    delete wait time

--- module_3/homework/test_items.py
+++ module_3/homework/test_items.py
@@ -4,5 +4,4 @@ import time
 def test_add_to_card_button_enable(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
     browser.find_element_by_class_name('btn-add-to-basket')
